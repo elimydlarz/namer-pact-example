71e13c6fd4486be8e4801339402e84453f7e6d91
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-03-03 15:57:31 +1100

    Removed erroneous RestClient junk.

--- spec/namer_spec.rb
+++ spec/namer_spec.rb
@@ -4,8 +4,6 @@ describe 'Namer'  do
   describe 'landing page' do
     subject { get '/' }
 
-    before { allow(RestClient).to receive(:get).with(ENV['NAME_PROVIDER_URL']).and_return('world') }
-
     its(:status) { is_expected.to eq 200 }
     its(:body) { is_expected.to eq 'world' }
   end
